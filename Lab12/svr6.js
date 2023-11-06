f9cfe4025da045bd76dc00efa44bd4de05ddf882
christopherrtan <crtan@hawaii.edu>
2023-11-05 17:52:18 -1000

    Lab12 Files
    Lab 12 Files

--- Lab12/svr6.js
+++ Lab12/svr6.js
@@ -1,3 +1,5 @@
+//svr.6.js
+
 let express = require('express');
 let app = express();
 
@@ -10,20 +12,36 @@ app.get('/test', function (req, res) {
     console.log('app.get for test was executed');
 });
 
+let products = require(__dirname + '/products.json');
+products.forEach( (prod,i) => {prod.total_sold = 0});
+
+app.get("/products.js", function (request, response, next) {
+   response.type('.js');
+   let products_str = `let products = ${JSON.stringify(products)};`;
+   response.send(products_str);
+});
+
 app.use(express.urlencoded({ extended: true }));
 
-
 app.post("/process_form", function (request, response) {
-    //response.send(request.body); 
-    let q = Number(request.body['qty_textbox']);
-    console.log("the input value is ..." +q);
-    let validationMessage = validateQuantity(q);
-
-    if (validationMessage === "") {
-        response.send(`Thank you for purchasing ${q} things!`);
-    } else {
-        response.send(validationMessage+'<br>'+`Error: ${q} is not a quantity. Hit the back button to fix.`);
+    let receipt = '';
+    let qtys = request.body[`quantity_textbox`];
+    console.log(qtys);
+    for (let i in qtys) {
+        let q = Number(qtys[i]);
+        console.log("the quantity value is " + q);
+        let validationMessage = validateQuantity(q);
+        let brand = products[i]['brand'];
+        let brand_price = products[i]['price'];
+        if(validateQuantity(q)==="") {
+            products[i]['total_sold'] += Number(q);
+            receipt += `<h3>${q}Thank you for puchasing: ${q} ${brand}. Your total is \$${q * brand_price}!</h3>`; // render template string
+        } else {
+            receipt+= `<h3><font color="red">${q} is not a valid quantity for ${brand}!<br>${validationMessage}</font></h3>`;
+        }
     }
+    response.send(receipt);
+    response.end();
     
  });
 
@@ -31,6 +49,7 @@ app.all('*', function (request, response, next) {
     //response.send(request.method + ' to path ' + request.path);
     console.log(request.method + ' to path ' + request.path);
 });
+
 
 app.listen(8080, () => console.log(`listening on port 8080`)); // note the use of an anonymous function here to do a callback
 
